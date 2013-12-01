d897e322eefdf4406ca0368e6e88c867e92608f5
Robert Berry <rjberry@gmail.com>
2013-12-01 21:18:36 +0000

    Add logic for next iteration of grid

--- GameOfLife.hs
+++ GameOfLife.hs
@@ -3,11 +3,12 @@
 import Data.Array.IArray
 import System.Random
 import Control.Applicative
+import Control.Concurrent
 import Control.Monad.State
 import Control.Monad
 import Data.List
 
-newtype Grid = Grid { cells :: (Array (Int, Int) Bool) }
+newtype Grid = Grid { cells :: Array (Int, Int) Bool }
   deriving (Eq, Ord, Read)
 
 data Coordinate = Coordinate { x :: Int, y :: Int }
@@ -23,6 +24,8 @@ instance Show Grid where
           gridLines = [gridLine n | n <- [0..rows]]
           gridLine row = ('|' : [(if ((cells g) ! (column, row)) then '#' else ' ') | column <- [0..columns]]) ++ "|"
            
+msBetweenIterations = 2000
+
 -- Helpful wrappers for Random typeclass in State monad
 randomSt :: (RandomGen g, Random a) => State g a
 randomSt = state random
@@ -58,7 +61,39 @@ randomGrid (columns, rows) =
     let initializations = [((x c, y c), c `elem` livingCells) | c <- coordinates (columns, rows)] in
       return $ Grid $ array ((0, 0), (columns, rows)) initializations
 
+neighbours :: Array (Int, Int) a -> Array (Int, Int) [a]
+neighbours a = array (bounds a) (map (\c -> (c, neighboursFor c)) (indices a))
+  where ((minX, minY), (maxX, maxY)) = bounds a
+        wrap :: Int -> Int -> Int -> Int
+        wrap min max n = mod (n - min) (max - min) + min
+        wrapX = wrap minX maxX
+        wrapY = wrap minY maxY
+        neighbourIndices x y = [((wrapX n), (wrapY n)) | (n, m) <-
+                                   [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]]
+        neighboursFor (x, y) = map (a !) (neighbourIndices x y)
+        
+-- Lives on in the next generation given whether alive now and how many neighbours alive
+lives :: Bool -> Int -> Bool
+lives True 2 = True
+lives _ 3 = True
+lives True _ = False
+lives _ _ = False
+
+argAnd :: (a -> b) -> a -> (a, b)
+argAnd f a = (a, f a)
+
+-- Next iteration of the grid
+nextGrid :: Grid -> Grid
+nextGrid (Grid a) = Grid $ array (bounds a) (map (argAnd alive) (indices a))
+                    where alive coord = lives (a ! coord) (neighboursAlive ! coord)
+                          neighboursAlive = amap (length . filter id) (neighbours a)
+
+printLoop :: Grid -> IO ()
+printLoop grid = do 
+  print grid
+  printLoop (nextGrid grid)
+
 main = do
   randomGen <- getStdGen
-  let (grid, _) = runState (randomGrid (10, 10)) randomGen
-      in putStrLn $ show grid
+  let (initialGrid, _) = runState (randomGrid (10, 10)) randomGen in
+    printLoop initialGrid
--- README.markdown
+++ README.markdown
@@ -3,5 +3,3 @@
 My attempt at an implementation of
 [Conway's Game of Life](http://en.wikipedia.org/wiki/Conway%27s_Game_of_Life)
 in [Haskell](http://www.haskell.org/haskellwiki/Haskell).
-
-Code is pretty crappy but I'm pretty new to Haskell. :-)
